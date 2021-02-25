f71292013098c2675097d8b7910ee311edbe98f7
ivn.omega <iwanaev23@gmail.com>
2021-02-25 15:46:09 +0300

    Added logic save data

--- lib/logic/services/database.dart
+++ lib/logic/services/database.dart
@@ -1,13 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  Future writeData(
+  Future writeIncome(
       String nameIncome, String comment, double sum, String idUser) async {
     await FirebaseFirestore.instance.collection('Income').add({
       'idUser': '$idUser',
       'nameIncome': '$nameIncome',
       'sumIncome': '$sum',
-      'comment': '$comment'
+      'commentIncome': '$comment'
+    });
+  }
+
+  Future writeCosts(
+      String nameIncome, String comment, double sum, String idUser) async {
+    await FirebaseFirestore.instance.collection('Costs').add({
+      'idUser': '$idUser',
+      'nameCosts': '$nameIncome',
+      'sumCosts': '$sum',
+      'commentCosts': '$comment'
     });
   }
 }
