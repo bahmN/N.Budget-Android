570100613023df37f102850915a8fa82d4d153d1
ivn.omega <iwanaev23@gmail.com>
2021-03-01 15:34:03 +0300

    Added read data

--- lib/logic/services/database.dart
+++ lib/logic/services/database.dart
@@ -5,24 +5,56 @@ final FirebaseAuth auth = FirebaseAuth.instance;
 
 class DatabaseService {
   Future writeIncome(String nameIncome, String comment, double sum) async {
-    final User user = auth.currentUser;
-    final idUser = user.uid;
+    final User _user = auth.currentUser;
+    final _idUser = _user.uid;
     await FirebaseFirestore.instance.collection('Income').add({
-      'idUser': '$idUser',
+      'idUser': '$_idUser',
       'nameIncome': '$nameIncome',
-      'sumIncome': '$sum',
+      'sumIncome': sum,
       'commentIncome': '$comment'
     });
   }
 
   Future writeCosts(String nameIncome, String comment, double sum) async {
-    final User user = auth.currentUser;
-    final idUser = user.uid;
+    final User _user = auth.currentUser;
+    final _idUser = _user.uid;
     await FirebaseFirestore.instance.collection('Costs').add({
-      'idUser': '$idUser',
+      'idUser': '$_idUser',
       'nameCosts': '$nameIncome',
-      'sumCosts': '$sum',
+      'sumCosts': sum,
       'commentCosts': '$comment'
     });
+  }
+}
+
+Future readCosts() async {
+  try {
+    final User _user = auth.currentUser;
+    final _idUser = _user.uid;
+
+    final _costs = await FirebaseFirestore.instance
+        .collection('Costs')
+        .where('_idUser', isEqualTo: _idUser)
+        .get();
+    return _costs.docs.fold<double>(0.0,
+        (previousValue, element) => previousValue + element.get('sumCosts'));
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future<double> readIncome() async {
+  try {
+    final User _user = auth.currentUser;
+    final _idUser = _user.uid;
+
+    final _income = await FirebaseFirestore.instance
+        .collection('Income')
+        .where('idUser', isEqualTo: _idUser)
+        .get();
+    return _income.docs.fold<double>(0.0,
+        (previousValue, element) => previousValue + element.get('sumIncome'));
+  } catch (e) {
+    print(e);
   }
 }
