fe015503620ddad537471072018352843fddc875
ivn.omega <iwanaev23@gmail.com>
2021-03-10 13:08:52 +0300

    New version application

--- lib/domains/myUser.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class MyUser {
-  String id;
-
-  MyUser.fromFirebase(User user) {
-    id = user.uid;
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hexcolor/hexcolor.dart';
-import 'package:nbudget/domains/myUser.dart';
-import 'package:nbudget/logic/services/auth.dart';
-import 'package:nbudget/screens/landing.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,19 +13,6 @@ class NBudget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<MyUser>.value(
-      value: AuthService().currentUser,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: [GlobalMaterialLocalizations.delegate],
-        supportedLocales: [
-          const Locale('en'),
-          const Locale('ru'),
-        ],
-        title: 'NBudget',
-        theme: ThemeData(primaryColor: HexColor('#FFE60D')),
-        home: LandingPage(),
-      ),
-    );
+    return Container();
   }
 }
